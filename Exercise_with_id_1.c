b49336cde39ebbd02db15513a678dec640d6aac7
kiritos <76980297+kiritoss316@users.noreply.github.com>
2022-06-02 17:29:43 -0300

    Update Exercise_with_id_1.c
    Remove variables  useless

--- Exercise_with_id_1.c
+++ Exercise_with_id_1.c
@@ -10,17 +10,16 @@ Find the sum of all the multiples of 3 or 5 below 1000
 
 //variables
 
-int a=0,b[1000],c=0,d=0,f=0;
+int b[1000],d=0,f=0;
 
 int main(int argc, char** argv) {
     while(d<1000){
         
         if(d%3==0||d%5==0){
-           
             b[d]=d;
             if(b[d]!=0){
                 f=f+b[d];
-                a++;
+                
             }
             
             
